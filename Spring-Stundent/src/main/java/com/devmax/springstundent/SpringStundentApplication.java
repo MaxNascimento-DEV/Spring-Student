aa35f6de6dc614c6f2271870c09c26c8c8d64dcf
MaxNascimento-DEV <maxwellcadete9@gmail.com>
2026-06-03 10:45:21 -0300

    Atualização-De-Projeto

---
+++ Spring-Stundent/src/main/java/com/devmax/springstundent/SpringStundentApplication.java
@@ -0,0 +1,13 @@
+package com.devmax.springstundent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStundentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStundentApplication.class, args);
+    }
+
+}
---
+++ Spring-Stundent/src/test/java/com/devmax/springstundent/SpringStundentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmax.springstundent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStundentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
